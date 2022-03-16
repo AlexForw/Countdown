eba97d0aa91c0c26c13fb2cd34e48547d208ae25
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-16 13:04:54 +0300

    feat: add time in milliseconds

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -4,5 +4,10 @@ const countdown = () => {
     const currentTime = new Date().getTime()
     const subtractionTime = countDate - currentTime
     
+
+    const sec = 1000
+    const minute = sec * 60
+    const hour = minute * 60
+    const day = hour * 24
 }
 countdown()
