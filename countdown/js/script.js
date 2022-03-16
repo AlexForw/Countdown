3ceed2639c8827c22ee43c020b4505fcf7d25aaa
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-16 12:54:25 +0300

    feat: add date, time expressions

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -1,1 +1,8 @@
 `use strict`
+const countdown = () => {
+    const countDate = new Date(2023, 0).getTime()
+    const currentTime = new Date().getTime()
+    const subtractionTime = countDate - currentTime
+    
+}
+countdown()
