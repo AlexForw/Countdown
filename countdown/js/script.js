e2b54c4511ded0d32e58c749d2baf30c42ff2e1d
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-16 14:37:15 +0300

    feat: add comments in code

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -1,24 +1,24 @@
 `use strict`
 const countdown = () => {
-    const countDate = new Date(2023, 0).getTime()
-    const currentTime = new Date().getTime()
-    const subtractionTime = countDate - currentTime
+    const countDate = new Date(2023, 0).getTime()       //  set time to New Year
+    const currentTime = new Date().getTime()            // set current time
+    const subtractionTime = countDate - currentTime     // define difference 
     
 
-    const second = 1000
-    const minute = second * 60
-    const hour = minute * 60
-    const day = hour * 24
+    const second = 1000                 // define times
+    const minute = second * 60          
+    const hour = minute * 60            
+    const day = hour * 24               
 
 
-    const countDay = Math.floor(subtractionTime / day)
+    const countDay = Math.floor(subtractionTime / day)                          // define times in milliseconds
     const countHour = Math.floor((subtractionTime % day) / hour)
     const countMinute = Math.floor((subtractionTime % hour) / minute)
     const countSecond = Math.floor((subtractionTime % minute) / second)
 
 
-    const dayCell = document.querySelector('.day')
-    dayCell.textContent = countDay
+    const dayCell = document.querySelector('.day')                              // find cells 
+    dayCell.textContent = countDay                                              // set times into cells
 
     const hourCell = document.querySelector('.hour')
     hourCell.textContent = countHour
@@ -30,4 +30,4 @@ const countdown = () => {
     secondCell.textContent = countSecond
 
 }
-setInterval(countdown,1000) 
+setInterval(countdown,1000)               // define interval for function (like online working)
